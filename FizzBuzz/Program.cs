8e4bd38b03565e1a81f188910044136170cc76b7
SaymanDotNet <saymannsk@gmail.com>
2025-03-14 13:11:50 +0700

    Добавьте файлы проекта.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,20 @@
+﻿
+FizzBuzz();
+
+Console.ReadLine();
+
+
+
+void FizzBuzz()
+{
+	foreach (var number in Enumerable.Range(1, 100))
+	{
+		Console.WriteLine(number switch
+		{
+			_ when number % 3 == 0 && number % 5 == 0 => "FizzBuzz",
+			_ when number % 3 == 0 => "Fizz",
+			_ when number % 5 == 0 => "Buzz",
+			_ => number.ToString()
+		});
+	}
+}
